989b0d98b260b955566c7d21f904423c5831c753
Eduardo Miguel Bennaton Handal <eduardohandal2@gmail.com>
2024-01-16 18:17:03 +0100

    Medium update
    - Changed general box dynamic to match selection choice

--- js/index.js
+++ js/index.js
@@ -1,41 +1,18 @@
-// Function to toggle content visibility
-function toggleContent(element) {
-    let isHidden = false;
-    const hiddenMessage = element.querySelector('#Hidden');
-    const display = element.querySelector('#Display');
-    const body = element.querySelector('#Body');
-
-    element.addEventListener('click', function () {
-        if (isHidden) {
-            element.style.backgroundColor = '#FFF';
-            element.style.boxShadow = '0px 0px 14px 0px rgba(0, 0, 0, 0.11)';
-            
-            hiddenMessage.style.display = 'none';
-            display.style.display = 'block';
-            if (body){
-                body.style.display = 'block';
-            }
-            
-        } else {
-            element.style.backgroundColor = '#B3B3B3';
-            element.style.boxShadow = '0px 0px 14px 0px rgba(0, 0, 0, 0.21)';
-            hiddenMessage.style.display = 'block';
-            display.style.display = 'none';
-
-            if (body){
-                body.style.display = 'none';
-            }
-            
-        }
-        isHidden = !isHidden;
-    });
-}
-
 // Get and toggle content for each button
 const buttons = document.querySelectorAll('.box-container');
+var currentSelected = null
+
 buttons.forEach(button => {
-    toggleContent(button);
-});
+    button.addEventListener('click', () => {
+        if (currentSelected != null) {
+            currentSelected.style.backgroundColor = 'var(--color-5)';
+            currentSelected.style.boxShadow = '0px 0px 0px 0px rgba(0, 0, 0, 0)';
+        }
+        currentSelected = button
+        button.style.backgroundColor = '#B3B3B3';
+        button.style.boxShadow = '0px 0px 14px 0px rgba(0, 0, 0, 0.11)';
+    })
+})
 
 // URL new-tab definition and icon generator
 const instagram = document.getElementById('instagram')
